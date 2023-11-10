d70b5bb1c4bd0f5a03555cda42d8fd3723b2e5a7
Sameema <sameemanasrin23@gmail.com>
2023-11-10 14:44:15 +0530

    hex animation

--- src/app/Components/about/about.component.ts
+++ src/app/Components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { delay } from 'rxjs';
 import { AppConfigs } from 'src/app/configs/AppConfigs';
 
 @Component({
@@ -9,19 +10,62 @@ import { AppConfigs } from 'src/app/configs/AppConfigs';
 export class AboutComponent {
   experience: any[] = [];
   education: any[] = [];
+  experienceImg: any[] = [];
+  educationImg: any[] = [];
 
-  constructor(private config: AppConfigs) {}
+  constructor(private config: AppConfigs) { }
 
   ngOnInit(): void {
     this.loadExpList();
     this.loadEduList();
+    this.loadExpImgList();
+    this.loadEduImgList();
   }
 
-  loadExpList():void {
+  loadExpList(): void {
     this.experience = this.config.ExperienceList;
   }
 
-  loadEduList():void {
+  loadEduList(): void {
     this.education = this.config.EducationList;
+  }
+
+  loadExpImgList(): void {
+    this.experienceImg = this.config.ExperienceImgList;
+  }
+
+  loadEduImgList(): void {
+    this.educationImg = this.config.EducationImgList;
+  }
+
+  changeImage(i: number, expedu: string): void {
+    var element = document.getElementById('project-hex');
+    if (element) {
+      element.style.backgroundImage = (expedu === 'exp') ? "url('" + this.experienceImg[i] + "')" : element.style.backgroundImage = "url('" + this.educationImg[i] + "')";
+      element.style.backgroundPosition = "center center";
+    } else {
+      console.log("Element with ID 'project-image-tag' not found");
+    }
+  }
+
+  defaultImage(): void {
+    var element = document.getElementById('project-hex');
+    if (element) {
+      element.style.backgroundPosition = "center 2000%";
+    } else {
+      console.log("Element with ID 'project-image-tag' not found");
+    }
+  }
+
+  setDefaultImage(): void {
+    var element = document.getElementById('project-hex');
+    if (element) {
+      setTimeout((element: HTMLElement) => {
+        element.style.backgroundImage = "url('../../../assets/profilePic1.jpg')";
+        element.style.backgroundPosition = "center center";
+      }, 50, element);
+    } else {
+      console.log("Element with ID 'project-image-tag' not found");
+    }
   }
 }
--- src/app/Components/projects/projects.component.ts
+++ src/app/Components/projects/projects.component.ts
@@ -6,20 +6,47 @@ import { AppConfigs } from '../../configs/AppConfigs';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-  ProjectList:any[] = [];
+  ProjectList: any[] = [];
   projectCount: number = 0;
-  constructor(private config: AppConfigs) {}
+  constructor(private config: AppConfigs) { }
 
   ngOnInit(): void {
     this.loadProjectList();
   }
 
-  loadProjectList():void {
+  loadProjectList(): void {
     this.ProjectList = this.config.ProjectList;
     this.projectCount = this.ProjectList.length
   }
 
-  changeImage(projectImg:string):void {
-    document.getElementById('project-image-tag')?.setAttribute('src', projectImg);
+  changeImage(projectImg: string): void {
+    var element = document.getElementById('project-hex');
+    if (element) {
+      element.style.backgroundImage = "url('" + projectImg + "')";
+      element.style.backgroundPosition = "center center";
+    } else {
+      console.log("Element with ID 'project-image-tag' not found");
+    }
+  }
+
+  defaultImage(): void {
+    var element = document.getElementById('project-hex');
+    if (element) {
+      element.style.backgroundPosition = "center 200%";
+    } else {
+      console.log("Element with ID 'project-image-tag' not found");
+    }
+  }
+
+  setDefaultImage(): void {
+    var element = document.getElementById('project-hex');
+    if (element) {
+      setTimeout((element: HTMLElement) => {
+        element.style.backgroundImage = "url('../../../assets/projectDefault.svg')";
+        element.style.backgroundPosition = "center center";
+      }, 50, element);
+    } else {
+      console.log("Element with ID 'project-image-tag' not found");
+    }
   }
 }
