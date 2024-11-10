70cc2a795711f0a128fce3596d0ca1ae3d685ee2
Sameema Nasrin <sameemanasrin23@gmail.com>
2024-11-10 19:24:44 +0530

    portfolio refined

--- src/app/configs/AppConfigs.ts
+++ src/app/configs/AppConfigs.ts
@@ -1,46 +1,64 @@
 export class AppConfigs {
   ProjectList: any[] = [
     {
+      name: 'Design Studio: Dzinr',
+      desc: "The dzinr Studio is a forward-thinking design studio that brings creative visions to life with sleek and engaging interfaces. This design-focused project combines cutting-edge aesthetics with seamless functionality to deliver visually compelling digital experiences. It's all about crafting user-centered designs that resonate and leave a lasting impact.",
+      techStack: ['Angular', 'Angular Material', 'Angular Router', 'RxJs'],
+      link: 'https://sameemanasrin.github.io/dzinr/',
+      img: 'assets/dzinr.png',
+    },
+    {
+      name: 'Design Studio: Bubbles',
+      desc: 'The Bubbles Studio is a dynamic and playful development environment that makes interactive design fun and functional. Specializing in building intuitive and engaging applications, this studio brings ideas to life with vibrant visuals and smooth interactions. It’s the perfect blend of innovation and creativity, designed to captivate users and enhance digital experiences.',
+      techStack: ['Angular', 'Angular Material', 'Angular Router', 'RxJs'],
+      link: 'https://sameemanasrin.github.io/bubbles/',
+      img: 'assets/bubbles.png',
+    },
+    {
       name: 'Multi Step Form',
       desc: 'The Multi Step Form UI project is a user-friendly and intuitive solution for collecting structured data from users in a step-by-step manner. This dynamic web-based form simplifies complex data input and enhances the user experience. With a responsive design and a focus on user guidance, the Multi Step Form ensures that data collection is both efficient and error-free.',
-      techStack: [ 'HTML', 'CSS', 'JavaScript'],
+      techStack: ['HTML', 'CSS', 'JavaScript'],
       link: 'https://sameemanasrin.github.io/multi-step-form/',
-      img: 'assets/multi-step-form.svg'
+      img: 'assets/multi-step-form.svg',
     },
     {
       name: 'Rock Paper Scissors Game',
-      desc: 'The Rock Paper Scissors Game UI project is a classic and engaging web-based game that pits a user against a computer in a battle of wits and strategy. This interactive game brings the timeless hand game to life with a user-friendly interface and captivating gameplay. It\'s a test of quick thinking, decision-making, and a dash of luck.',
-      techStack: [ 'HTML', 'CSS', 'JavaScript'],
+      desc: "The Rock Paper Scissors Game UI project is a classic and engaging web-based game that pits a user against a computer in a battle of wits and strategy. This interactive game brings the timeless hand game to life with a user-friendly interface and captivating gameplay. It's a test of quick thinking, decision-making, and a dash of luck.",
+      techStack: ['HTML', 'CSS', 'JavaScript'],
       link: 'https://sameemanasrin.github.io/rock_paper_scissors_game/',
-      img: 'assets/rock-paper-scissors.svg'
-    }
+      img: 'assets/rock-paper-scissors.svg',
+    },
   ];
 
-  ExperienceList: any = [{
+  ExperienceList: any = [
+    {
       title: 'Senior Software Engineer',
       company: 'Capgemini',
       duration: '2021-Present',
-    }//,
+    }, //,
     // {
     //   title: 'Software Engineer',
     //   company: 'Capgemini',
     //   duration: '2021-2023'
-    // } 
-  ]
+    // }
+  ];
 
   ExperienceImgList: any = [
-    'assets/multi-step-form.svg', 'assets/rock-paper-scissors.svg'
-  ]
+    'assets/multi-step-form.svg',
+    'assets/rock-paper-scissors.svg',
+  ];
 
-  EducationList: any = [{
+  EducationList: any = [
+    {
       course: 'B Tech from GNIT',
       duration: '2017-2021',
-      college: 'Gurunanak Institue of technology, Kolkata, WB'
-    }
-  ]
+      college: 'Gurunanak Institue of technology, Kolkata, WB',
+    },
+  ];
 
   EducationImgList: any = [
-    'assets/multi-step-form.svg', 'assets/rock-paper-scissors.svg', 'assets/multi-step-form.svg'
-  ]
+    'assets/multi-step-form.svg',
+    'assets/rock-paper-scissors.svg',
+    'assets/multi-step-form.svg',
+  ];
 }
-
